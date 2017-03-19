74c1d57630eb4118c5d16aa4b1e04f21933ea986
ylin10 <ylin10@bu.edu>
2017-03-19 19:32:21 -0400

    Updated Everything

--- APIprototype/server.js
+++ APIprototype/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser=require('body-parser');
 
 console.log("App is up and running!");
 
@@ -11,3 +12,8 @@ app.get("/", function(req, res) {
   console.log("User requesting/landing on homepage.");
   res.sendFile(__dirname + "/views/home.html");
 });
+
+/*app.get("/apiresult", function(req, res) {
+  console.log("Returning the API results.");
+  res.send(req.query);
+});*/

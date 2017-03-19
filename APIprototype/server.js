5c460b4433026617de35dd117cb6c5ea899778b9
jessicalberto <koualoha@bu.edu>
2017-03-19 19:26:57 -0400

    Upload base foundation for API prototype

---
+++ APIprototype/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+console.log("App is up and running!");
+
+app.listen(8000, function() {
+  console.log("Running and listening on PORT 8000");
+})
+
+app.get("/", function(req, res) {
+  console.log("User requesting/landing on homepage.");
+  res.sendFile(__dirname + "/views/home.html");
+});

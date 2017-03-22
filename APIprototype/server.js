84adc45c60517eaf476542a951c9b85cd9028f28
ylin10 <ylin10@bu.edu>
2017-03-21 22:48:16 -0400

    Updated for Presentation Tomorrow
    Added Yelp API Integration, with GET request from HTML form, to return
    the first primary result and all other parameters based on user’s
    search + location in box.

--- APIprototype/server.js
+++ APIprototype/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser=require('body-parser');
+const yelp = require('yelp-fusion');
+
+'use strict';
+
+
+const token = yelp.accessToken("nicetry", "scumbagz").then(response => {
+const client = yelp.client(response.jsonBody.access_token);
 
 console.log("App is up and running!");
 
@@ -13,7 +20,30 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/home.html");
 });
 
-/*app.get("/apiresult", function(req, res) {
-  console.log("Returning the API results.");
-  res.send(req.query);
-});*/
+
+app.get("/yelpresult", function(req, res) {
+  console.log("SEARCH = " + req.query.search);
+  console.log("LOCATION = " + req.query.location);
+
+    client.search({
+      term: req.query.search,
+      location: req.query.location
+    }).then (response => {
+      console.log(response.jsonBody.businesses[0].name);
+      var tophit = response.jsonBody.businesses[0].name;
+      var biz_id = response.jsonBody.businesses[0].id;
+
+      //we're sending just back to first business that we get with the result with all the params
+      res.send(response.jsonBody.businesses[0]);
+
+    client.reviews(biz_id).then (response => {
+      console.log(response.jsonBody.reviews);
+      //playing around with sending back reviews, couldn't get them to go together so w/e for this round
+      var biz_reviews = response.jsonBody.reviews;
+    })
+
+    })
+
+  })
+
+});

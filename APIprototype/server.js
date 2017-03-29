71f51d08831736ef4905ef22078fe4406ffc900b
ylin10 <ylin10@bu.edu>
2017-03-29 13:58:17 -0400

    Added comments to easier to follow the block, updated home.html for Twitter login button
    - Button on homepage just routes to the set twitter login to get oath
    rolling.  Button is just an omg src with a href that links to the newly
    created route.
    - Added comments and dividers throughout each block code in the
    server.js so easier to navigate, understand what’s going on

--- APIprototype/server.js
+++ APIprototype/server.js
@@ -11,14 +11,16 @@ var keys = require('./config');
 
 var myKey = keys.mykey;
 var secretKey= keys.secretkey;
-var myconsumerKey = keys.twitterkey;
-var myconsumerSecret = keys.twittersecret;
+//var myconsumerKey = keys.twitterkey;
+//var myconsumerSecret = keys.twittersecret;
+
+//Using the npm package of passport/twitter strategy to setup our Twitter oauth
 
 passport.use(new Strategy ({
-  consumerKey: myconsumerKey,
-  consumerSecret: myconsumerSecret,
-  callbackURL:'http://localhost:8000/'
-},
+  consumerKey: "help",
+  consumerSecret: "jessica",
+  callbackURL:'http://localhost:8000/' //this represents the callback URL after successful oauth, where twitter redirects the user to
+},//not so sure about shit below ,but it works
   function(token, tokenSecret, profile, cb) {
     console.log("user made it to this stage of function!");
     return cb(null, profile);
@@ -34,28 +36,41 @@ passport.deserializeUser(function(obj, cb) {
 
 app.use(require('express-session')({secret: 'placeholder', resave: true, saveUninitialized:true}));
 
-
 'use strict';
+
+//Authentication for Yelp API below
 
 const token = yelp.accessToken(myKey, secretKey).then(response => {
 const client = yelp.client(response.jsonBody.access_token);
 
+//Basic web app startup and routing
+
 console.log("App is up and running!");
+
+//Listens on port 8000 for a request
 
 app.listen(8000, function() {
   console.log("Running and listening on PORT 8000");
 })
+
+//Sets the route for the home page, logs once in terminal msg, sendFiles of our home.html
 
 app.get("/", function(req, res) {
   console.log("User requesting/landing on homepage.");
   res.sendFile(__dirname + "/views/home.html");
 });
 
+//Initializes a session needed for our Twitter oauth
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Sets the page that links to the loginto twitter authentication page
+
 app.get("/login/twitter",
   passport.authenticate('twitter'));
+
+//This is the Yelp API result after user fills out form and submits query
 
 app.get("/yelpresult", function(req, res) {
   console.log("SEARCH = " + req.query.search);
